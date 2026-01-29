9dd84992eabd5fecc6ebf4b711db6434a18dffb2
Pedro Madruga <pedro.madruga@liferay.com>
2026-01-29 11:12:41 -0300

    Section 009 - requisicao fetch GET com parametros -> PT. 2

--- SECCAO+09+-+JSON+E+FETCH+API/SECCAO 09 - JSON E FETCH API/009_requisicao_fetch_GET_com_parametros_2/script.js
+++ SECCAO+09+-+JSON+E+FETCH+API/SECCAO 09 - JSON E FETCH API/009_requisicao_fetch_GET_com_parametros_2/script.js
@@ -6,75 +6,74 @@ const btn = document.querySelector("#btn");
 const informacao = document.querySelector("#informacao");
 const erro = document.querySelector("#erro");
 
-btn.addEventListener('click', () => {
+btn.addEventListener("click", () => {
+  // vamos validar a data
+  const data = document.querySelector("#data_input");
+  let state = {
+    state: "ok",
+    message: "",
+  };
 
-    // vamos validar a data
-    const data = document.querySelector("#data_input");
-    let state = {
-        state: "ok",
-        message: ""
-    }
+  // validar se data está vazio
+  if (data.value === "") {
+    state.state = "error";
+    state.message = "Erro na data - data vazia";
+    showError(state);
+    return;
+  }
 
-    // validar se data está vazio
-    if(data.value === ""){
-        state.state = "error";
-        state.message = "Erro na data - data vazia";
-        showError(state);
-        return;
-    }
+  // verificar se a data é válida
+  if (isNaN(Date.parse(data.value))) {
+    state.state = "error";
+    state.message = "Erro na data - data inválida";
+    showError(state);
+    return;
+  }
 
-    // verificar se a data é válida
-    if(isNaN(Date.parse(data.value))){
-        state.state = "error";
-        state.message = "Erro na data - data inválida";
-        showError(state);
-        return;
-    }
+  // verifica se a data é maior do que a data atual
+  const dataAtual = new Date();
+  if (new Date(data.value) > dataAtual) {
+    state.state = "error";
+    state.message = "Erro na data - data superior ao dia atual";
+    showError(state);
+    return;
+  }
 
-    // verifica se a data é maior do que a data atual
-    const dataAtual = new Date();
-    if(new Date(data.value) > dataAtual){
-        state.state = "error";
-        state.message = "Erro na data - data superior ao dia atual";
-        showError(state);
-        return;
-    }
+  // vamos preparar a URL para ter o número de parâmetros que desejamos
+  // https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=2023-10-01
+  let parametros = new URLSearchParams({
+    api_key: "DEMO_KEY",
+    date: data.value,
+  });
 
-    // vamos preparar a URL para ter o número de parâmetros que desejamos
-    // https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=2023-10-01
-    let parametros = new URLSearchParams({
-        api_key: "DEMO_KEY",
-        date: data.value
-    });
+  let url = "https://api.nasa.gov/planetary/apod?" + parametros.toString();
 
-    let url = "https://api.nasa.gov/planetary/apod?" + parametros.toString();
-
-    fetch(url)
+  fetch(url)
     .then((response) => {
-        if (!response.ok) {
-            throw new Error("Network response was not ok");
-        }
-        return response.json();
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return response.json();
     })
     .then((data) => {
-        document.querySelector("#imagem").src = data.url; // A imagem do dia
-        document.querySelector("#titulo").innerHTML = data.title; // O título
-        document.querySelector("#descricao").innerHTML = data.explanation; // A descrição
-        document.querySelector("#data").innerHTML = data.date; // A data
+      document.querySelector("#imagem").src = data.url; // A imagem do dia
+      document.querySelector("#titulo").innerHTML = data.title; // O título
+      document.querySelector("#descricao").innerHTML = data.explanation; // A descrição
+      document.querySelector("#data").innerHTML = data.date; // A data
 
-        // tornar visível a informação
-        informacao.classList.remove("hidden");
+      // tornar visível a informação
+      informacao.classList.remove("hidden");
     })
     .catch((error) => {
-        informacao.innerHTML = error;
+      informacao.innerHTML = error;
     });
 });
 
-function showError(state){
-    informacao.classList.add("hidden");
-    erro.innerHTML = state.message;
-    erro.classList.remove("hidden");
-    setTimeout(() => {
-        erro.classList.add("hidden");
-    }, 3000);
+function showError(state) {
+  informacao.classList.add("hidden");
+  erro.innerHTML = state.message;
+  erro.classList.remove("hidden");
+  setTimeout(() => {
+    erro.classList.add("hidden");
+  }, 3000);
 }
