b4b5682469f57b57a46ccae30f8db2fddc3aaa79
Pedro Madruga <pedro.madruga@liferay.com>
2026-01-29 11:26:17 -0300

    Adicionando request GET restfull api

--- SECCAO+09+-+JSON+E+FETCH+API/SECCAO 09 - JSON E FETCH API/011_request_get_no_restfull_api_dev/script.js
+++ SECCAO+09+-+JSON+E+FETCH+API/SECCAO 09 - JSON E FETCH API/011_request_get_no_restfull_api_dev/script.js
@@ -8,45 +8,42 @@ Vamos então testar o restful-api.dev.
 const resultado = document.querySelector("#resultado");
 
 // buscar a lista de objetos
-document.querySelector("#btn1").addEventListener('click', () => {
+document.querySelector("#btn1").addEventListener("click", () => {
+  fetch("https://api.restful-api.dev/objects")
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Aconteceu um erro");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      // exibir a coleção no console
+      console.log(data);
 
-    fetch("https://api.restful-api.dev/objects")
-        .then((response) => {
-            if(!response.ok){
-                throw new Error("Aconteceu um erro");
-            }
-            return response.json();
-        })
-        .then((data) => {
-
-            // exibir a coleção no console
-            console.log(data);
-
-            let lista = "<ul>";
-            data.forEach(item => {
-                lista += `<li>${item.name}</li>`
-            });
-            lista += "</ul>";
-            resultado.innerHTML = lista;
-        })
-        .catch((error) => {
-            resultado.innerHTML = error;
-        })
+      let lista = "<ul>";
+      data.forEach((item) => {
+        lista += `<li>${item.name}</li>`;
+      });
+      lista += "</ul>";
+      resultado.innerHTML = lista;
+    })
+    .catch((error) => {
+      resultado.innerHTML = error;
+    });
 });
 
 // buscar um objeto específico
-document.querySelector("#btn2").addEventListener('click', () => {
-
-    fetch("https://api.restful-api.dev/objects")
-        .then((response) => {
-            if(!response.ok){
-                throw new Error("Aconteceu um erro");
-            }
-            return response.json();
-        })
-        .then((data) => {
-            let objeto = data[0];
-            let detalhes = `
+document.querySelector("#btn2").addEventListener("click", () => {
+  fetch("https://api.restful-api.dev/objects")
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Aconteceu um erro");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      let objeto = data[0];
+      let detalhes = `
                 <h2>${objeto.name}</h2>
                 
                 <p>ID: ${objeto.id}</p>
@@ -54,10 +51,9 @@ document.querySelector("#btn2").addEventListener('click', () => {
                 <p>Capacidade: ${objeto.data.capacity}</p>
                 <p>Cor: ${objeto.data.color}</p>
             `;
-            resultado.innerHTML = detalhes;
-        })
-        .catch((error) => {
-            resultado.innerHTML = error;
-        })
-
+      resultado.innerHTML = detalhes;
+    })
+    .catch((error) => {
+      resultado.innerHTML = error;
+    });
 });
